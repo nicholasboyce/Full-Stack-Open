ea55e4aa958299e474a6e6f61ced714617569858
nicholasboyce <nicholasboyce@protonmail.com>
2024-01-11 13:29:30 -0500

    Exercise 1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -30,27 +30,29 @@ function Total(props) {
 }
 
 function App() {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Heading course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Heading course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
