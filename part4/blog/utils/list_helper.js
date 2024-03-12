c34f2f365e10312ef09f8070068676bee8ea020b
nicholasboyce <nicholasboyce@protonmail.com>
2024-03-12 14:53:42 -0400

    Exercises 4.6 and 4.7

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -32,8 +32,60 @@ const favoriteBlog = (blogs) => {
     return favorite;
 }
 
+const mostBlogs = (blogs) => {
+    const authors = new Map();
+    let maxBlogs = 0;
+    let mostBlogged = null;
+    for (let i = 0; i < blogs.length; i++) {
+        const currAuthor = blogs[i].author;
+        let authorBlogs;
+        if (authors.has(currAuthor)) {
+            authorBlogs = authors.get(currAuthor) + 1;
+            authors.set(currAuthor, authorBlogs);
+        } else {
+            authors.set(currAuthor, 1);
+            authorBlogs = 1;
+        }
+        if (authorBlogs > maxBlogs) {
+            maxBlogs = authorBlogs;
+            mostBlogged = {
+                author: currAuthor,
+                blogs: authorBlogs
+            }
+        }
+    }
+    return mostBlogged;
+}
+
+const mostLikes = (blogs) => {
+    const authors = new Map();
+    let maxLikes = 0;
+    let mostLiked = null;
+    for (let i = 0; i < blogs.length; i++) {
+        const currAuthor = blogs[i].author;
+        let currLikes = blogs[i].likes;
+        if (authors.has(currAuthor)) {
+            const authLikes = authors.get(currAuthor);
+            authors.set(currAuthor, authLikes + currLikes);
+            currLikes = authLikes + currLikes;
+        } else {
+            authors.set(currAuthor, currLikes);
+        }
+        if (currLikes > maxLikes) {
+            maxLikes = currLikes;
+            mostLiked = {
+                author: currAuthor,
+                likes: currLikes
+            }
+        }
+    }
+    return mostLiked;
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
