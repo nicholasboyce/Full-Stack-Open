d22e3853ff6d66801c85fb340e7a90f86e34dede
nicholasboyce <nicholasboyce@protonmail.com>
2024-03-16 22:31:36 -0400

    Added test for user creation.

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -2,11 +2,13 @@ const { test, after, describe, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const bcrypt = require('bcrypt')
 const app = require('../app')
 
 const api = supertest(app)
 const helper = require('./test_helper');
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 describe('when there are blog posts initially saved', () => {
 
@@ -213,6 +215,41 @@ describe('when there are blog posts initially saved', () => {
     });
 });
 
+describe('when there is initially one user in the DB', () => {
+    beforeEach(async () => {
+        await User.deleteMany({});
+
+        const passwordHash = await bcrypt.hash('sekret', 10);
+        const user = new User({
+            username: 'root',
+            passwordHash
+        });
+
+        await user.save();
+    });
+
+    test('creation succeeds with a fresh username', async () => {
+        const usersAtStart = await helper.usersInDB();
+
+        const newUser = {
+            username: 'sarah1',
+            name: 'sarahthekid',
+            password: 'abc123'
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(201)
+            .expect('Content-Type', /application\/json/);
+
+        const usersAtEnd = await helper.usersInDB();
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1);
+
+        const usernames = usersAtEnd.map(user => user.username);
+        assert(usernames.includes(newUser.username));
+    });
+});
 
 after(async () => {
     await mongoose.connection.close()
